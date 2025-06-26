e1912d185b5f0a83e5741aafb491494de792fcf7
Prabhveer Singh <hegakoi538@gmail.com>
2025-06-26 09:14:33 +0530

    text2imgPS333.py
    Updated 🔥

--- text2imgPS333.py
+++ text2imgPS333.py
@@ -10,41 +10,6 @@ st.set_page_config(
     layout="centered"
 )
 
-# Background image (custom)
-st.markdown(
-    f"""
-    <style>
-    .stApp::before {{
-        content: "";
-        position: absolute;
-        top: 0;
-        left: 0;
-        right: 0;
-        bottom: 0;
-        background-image: url("https://i.ibb.co/6Jfmhkh7/34e1a10894d62bce16ac68a92e98931e.jpg");
-        background-size: cover;
-        background-repeat: no-repeat;
-        background-position: center;
-        opacity: 0.15;  /* 👈 Thoda halka effect */
-        z-index: -1;
-    }}
-    .block-container {{
-        padding-top: 2rem;
-        padding-bottom: 2rem;
-        position: relative;
-        z-index: 1;
-    }}
-    input, textarea {{
-        font-size: 18px !important;
-    }}
-    button {{
-        font-size: 18px !important;
-    }}
-    </style>
-    """,
-    unsafe_allow_html=True
-)
-
 # Logo image and title
 st.image("https://i.ibb.co/5Wp2sxxn/1000319964-removebg-preview.png", width=250)
 st.title("🔥 PS333 Image Generator 🔥")
